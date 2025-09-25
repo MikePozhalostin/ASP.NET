267c65639bf8b71fe2fccdcb313b1c7627e4d5c5
Михаил Пожалостин <m.pozhalostin@whyhappen.ru>
2025-09-25 11:58:44 +0300

    add preferences controller

---
+++ Homeworks/EF/src/PromoCodeFactory.WebHost/Controllers/PreferenceController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PromoCodeFactory.Core.Abstractions.Repositories;
+using PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using PromoCodeFactory.WebHost.Models;
+
+namespace PromoCodeFactory.WebHost.Controllers
+{
+    /// <summary>
+    /// Предпочтения
+    /// </summary>
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class PreferenceController
+        : ControllerBase
+    {
+        private readonly IRepository<Preference> _preferenceRepository;
+
+        public PreferenceController(IRepository<Preference> preferenceRepository)
+        {
+            _preferenceRepository = preferenceRepository;
+        }
+
+        /// <summary>
+        /// Получить все предпочтения
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<List<PreferenceResponse>> GetPreferencesAsync()
+        {
+            var preferences = await _preferenceRepository.GetAllAsync();
+
+            return preferences.Select(x =>
+                new PreferenceResponse()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                }).ToList();
+        }
+    }
+}
